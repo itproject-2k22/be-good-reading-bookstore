f21c5f377d7470b3b7164fc7116bb8a82abf17bb
Awan <itproject2k22@gmail.com>
2024-06-06 10:20:37 +0700

    fix: TCL in Customers Table

--- models/customer.js
+++ models/customer.js
@@ -1,36 +1,86 @@
 const pool = require('../config/db');
 
 const getAllCustomers = async () => {
-    const result = await pool.query('SELECT * FROM customer');
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM customer');
+        await client.query('COMMIT');
+        return result.rows;
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getCustomerById = async (id) => {
-    const result = await pool.query('SELECT * FROM customer WHERE id = $1', [id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM customer WHERE id = $1', [id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const createCustomer = async (customer) => {
     const { id, name, email, phone_number } = customer;
-    const result = await pool.query(
-        'INSERT INTO customer (id, name, email, phone_number) VALUES ($1, $2, $3, $4) RETURNING *',
-        [id, name, email, phone_number]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'INSERT INTO customer (id, name, email, phone_number) VALUES ($1, $2, $3, $4) RETURNING *',
+            [id, name, email, phone_number]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const updateCustomer = async (id, customer) => {
     const { name, email, phone_number } = customer;
-    const result = await pool.query(
-        'UPDATE customer SET name = $1, email = $2, phone_number = $3 WHERE id = $4 RETURNING *',
-        [name, email, phone_number, id]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'UPDATE customer SET name = $1, email = $2, phone_number = $3 WHERE id = $4 RETURNING *',
+            [name, email, phone_number, id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const deleteCustomer = async (id) => {
-    const result = await pool.query('DELETE FROM customer WHERE id = $1 RETURNING *', [id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('DELETE FROM customer WHERE id = $1 RETURNING *', [id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 module.exports = {
