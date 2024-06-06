2985dadac61c67971f1f3147a854c8658ffb4d07
Awan <itproject2k22@gmail.com>
2024-06-06 10:20:50 +0700

    fix: TCL in Genres Table

--- models/genre.js
+++ models/genre.js
@@ -1,36 +1,86 @@
 const pool = require('../config/db');
 
 const getAllGenres = async () => {
-    const result = await pool.query('SELECT * FROM genre');
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM genre');
+        await client.query('COMMIT');
+        return result.rows;
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getGenreById = async (id) => {
-    const result = await pool.query('SELECT * FROM genre WHERE id = $1', [id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM genre WHERE id = $1', [id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const createGenre = async (genre) => {
     const { id, name } = genre;
-    const result = await pool.query(
-        'INSERT INTO genre (id, name) VALUES ($1, $2) RETURNING *',
-        [id, name]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'INSERT INTO genre (id, name) VALUES ($1, $2) RETURNING *',
+            [id, name]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const updateGenre = async (id, genre) => {
     const { name } = genre;
-    const result = await pool.query(
-        'UPDATE genre SET name = $1 WHERE id = $2 RETURNING *',
-        [name, id]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'UPDATE genre SET name = $1 WHERE id = $2 RETURNING *',
+            [name, id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const deleteGenre = async (id) => {
-    const result = await pool.query('DELETE FROM genre WHERE id = $1 RETURNING *', [id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('DELETE FROM genre WHERE id = $1 RETURNING *', [id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 module.exports = {
