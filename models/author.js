3b031d778888d07bdfd0fdb7a1cce6a83a4945e8
Awan <itproject2k22@gmail.com>
2024-06-06 08:21:37 +0700

    fix: add TCL into author table

--- models/author.js
+++ models/author.js
@@ -1,36 +1,86 @@
 const pool = require('../config/db');
 
 const getAllAuthors = async () => {
-    const result = await pool.query('SELECT * FROM authors');
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM authors');
+        await client.query('COMMIT');
+        return result.rows;
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getAuthorById = async (id) => {
-    const result = await pool.query('SELECT * FROM authors WHERE id = $1', [id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM authors WHERE id = $1', [id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const createAuthor = async (author) => {
     const { id, name, born_date } = author;
-    const result = await pool.query(
-        'INSERT INTO authors (id, name, born_date) VALUES ($1, $2, $3) RETURNING *',
-        [id, name, born_date]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'INSERT INTO authors (id, name, born_date) VALUES ($1, $2, $3) RETURNING *',
+            [id, name, born_date]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const updateAuthor = async (id, author) => {
     const { name, born_date } = author;
-    const result = await pool.query(
-        'UPDATE authors SET name = $1, born_date = $2 WHERE id = $3 RETURNING *',
-        [name, born_date, id]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'UPDATE authors SET name = $1, born_date = $2 WHERE id = $3 RETURNING *',
+            [name, born_date, id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const deleteAuthor = async (id) => {
-    const result = await pool.query('DELETE FROM authors WHERE id = $1 RETURNING *', [id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('DELETE FROM authors WHERE id = $1 RETURNING *', [id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 module.exports = {
