6b9ccdba601e7da76f22981e7edf6cc5f6475196
Awan <itproject2k22@gmail.com>
2024-06-06 10:21:52 +0700

    Fix: TCL in Reviews Table

--- models/reviews.js
+++ models/reviews.js
@@ -1,36 +1,86 @@
 const pool = require('../config/db');
 
 const getAllReviews = async () => {
-    const result = await pool.query('SELECT * FROM reviews');
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM reviews');
+        await client.query('COMMIT');
+        return result.rows;
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getReviewById = async (reviews_id) => {
-    const result = await pool.query('SELECT * FROM reviews WHERE reviews_id = $1', [reviews_id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM reviews WHERE reviews_id = $1', [reviews_id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const createReview = async (review) => {
     const { reviews_id, book_id, score, detail_review } = review;
-    const result = await pool.query(
-        'INSERT INTO reviews (reviews_id, book_id, score, detail_review) VALUES ($1, $2, $3, $4) RETURNING *',
-        [reviews_id, book_id, score, detail_review]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'INSERT INTO reviews (reviews_id, book_id, score, detail_review) VALUES ($1, $2, $3, $4) RETURNING *',
+            [reviews_id, book_id, score, detail_review]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const updateReview = async (reviews_id, review) => {
     const { book_id, score, detail_review } = review;
-    const result = await pool.query(
-        'UPDATE reviews SET book_id = $1, score = $2, detail_review = $3 WHERE reviews_id = $4 RETURNING *',
-        [book_id, score, detail_review, reviews_id]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'UPDATE reviews SET book_id = $1, score = $2, detail_review = $3 WHERE reviews_id = $4 RETURNING *',
+            [book_id, score, detail_review, reviews_id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const deleteReview = async (reviews_id) => {
-    const result = await pool.query('DELETE FROM reviews WHERE reviews_id = $1 RETURNING *', [reviews_id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('DELETE FROM reviews WHERE reviews_id = $1 RETURNING *', [reviews_id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 module.exports = {
