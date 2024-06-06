d1ee5cc103f93e539619ee41664fc79a049344c4
Awan <itproject2k22@gmail.com>
2024-06-06 10:21:10 +0700

    Fix: TCL in List Adrress Table

--- models/listAddress.js
+++ models/listAddress.js
@@ -1,36 +1,86 @@
 const pool = require('../config/db');
 
 const getAllAddresses = async () => {
-    const result = await pool.query('SELECT * FROM list_address');
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM list_address');
+        await client.query('COMMIT');
+        return result.rows;
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getAddressById = async (address_id) => {
-    const result = await pool.query('SELECT * FROM list_address WHERE address_id = $1', [address_id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM list_address WHERE address_id = $1', [address_id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const createAddress = async (address) => {
     const { address_id, customer_id, street, city, zipcode } = address;
-    const result = await pool.query(
-        'INSERT INTO list_address (address_id, customer_id, street, city, zipcode) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-        [address_id, customer_id, street, city, zipcode]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'INSERT INTO list_address (address_id, customer_id, street, city, zipcode) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+            [address_id, customer_id, street, city, zipcode]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const updateAddress = async (address_id, address) => {
     const { customer_id, street, city, zipcode } = address;
-    const result = await pool.query(
-        'UPDATE list_address SET customer_id = $1, street = $2, city = $3, zipcode = $4 WHERE address_id = $5 RETURNING *',
-        [customer_id, street, city, zipcode, address_id]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'UPDATE list_address SET customer_id = $1, street = $2, city = $3, zipcode = $4 WHERE address_id = $5 RETURNING *',
+            [customer_id, street, city, zipcode, address_id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const deleteAddress = async (address_id) => {
-    const result = await pool.query('DELETE FROM list_address WHERE address_id = $1 RETURNING *', [address_id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('DELETE FROM list_address WHERE address_id = $1 RETURNING *', [address_id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 module.exports = {
