8f73bc6078bc8424e20f161f197fbfa3890c1c61
Awan <itproject2k22@gmail.com>
2024-06-06 10:22:05 +0700

    Fix: TCL in Staffs Table

--- models/staffs.js
+++ models/staffs.js
@@ -1,36 +1,86 @@
 const pool = require('../config/db');
 
 const getAllStaffs = async () => {
-    const result = await pool.query('SELECT * FROM staffs');
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM staffs');
+        await client.query('COMMIT');
+        return result.rows;
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getStaffById = async (id) => {
-    const result = await pool.query('SELECT * FROM staffs WHERE id = $1', [id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM staffs WHERE id = $1', [id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const createStaff = async (staff) => {
     const { id, name, role, salary, store } = staff;
-    const result = await pool.query(
-        'INSERT INTO staffs (id, name, role, salary, store) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-        [id, name, role, salary, store]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'INSERT INTO staffs (id, name, role, salary, store) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+            [id, name, role, salary, store]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const updateStaff = async (id, staff) => {
     const { name, role, salary, store } = staff;
-    const result = await pool.query(
-        'UPDATE staffs SET name = $1, role = $2, salary = $3, store = $4 WHERE id = $5 RETURNING *',
-        [name, role, salary, store, id]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'UPDATE staffs SET name = $1, role = $2, salary = $3, store = $4 WHERE id = $5 RETURNING *',
+            [name, role, salary, store, id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const deleteStaff = async (id) => {
-    const result = await pool.query('DELETE FROM staffs WHERE id = $1 RETURNING *', [id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('DELETE FROM staffs WHERE id = $1 RETURNING *', [id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 module.exports = {
