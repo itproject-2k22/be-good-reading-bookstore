43ee5b837501fa8a3c3a78ebb436588b56845c25
Awan <itproject2k22@gmail.com>
2024-06-06 10:19:48 +0700

    fix: TCL in Book Release Table

--- models/bookRelease.js
+++ models/bookRelease.js
@@ -1,36 +1,86 @@
 const pool = require('../config/db');
 
 const getAllBookReleases = async () => {
-    const result = await pool.query('SELECT * FROM book_release');
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM book_release');
+        await client.query('COMMIT');
+        return result.rows;
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getBookReleaseById = async (book_id, author_id) => {
-    const result = await pool.query('SELECT * FROM book_release WHERE book_id = $1 AND author_id = $2', [book_id, author_id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM book_release WHERE book_id = $1 AND author_id = $2', [book_id, author_id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const createBookRelease = async (bookRelease) => {
     const { book_id, author_id, release_year } = bookRelease;
-    const result = await pool.query(
-        'INSERT INTO book_release (book_id, author_id, release_year) VALUES ($1, $2, $3) RETURNING *',
-        [book_id, author_id, release_year]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'INSERT INTO book_release (book_id, author_id, release_year) VALUES ($1, $2, $3) RETURNING *',
+            [book_id, author_id, release_year]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const updateBookRelease = async (book_id, author_id, bookRelease) => {
-    const { release_year } = bookRelease;
-    const result = await pool.query(
-        'UPDATE book_release SET release_year = $1 WHERE book_id = $2 AND author_id = $3 RETURNING *',
-        [release_year, book_id, author_id]
-    );
-    return result.rows[0];
+    const { new_book_id, new_author_id, release_year } = bookRelease;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'UPDATE book_release SET book_id = $1, author_id = $2, release_year = $3 WHERE book_id = $4 AND author_id = $5 RETURNING *',
+            [new_book_id, new_author_id, release_year, book_id, author_id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const deleteBookRelease = async (book_id, author_id) => {
-    const result = await pool.query('DELETE FROM book_release WHERE book_id = $1 AND author_id = $2 RETURNING *', [book_id, author_id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('DELETE FROM book_release WHERE book_id = $1 AND author_id = $2 RETURNING *', [book_id, author_id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 module.exports = {
