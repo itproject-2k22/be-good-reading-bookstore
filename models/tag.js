1f0c29ea4c100087f000056a348834eef3d6f74e
Awan <itproject2k22@gmail.com>
2024-06-06 10:22:16 +0700

    Fix: TCL in Tag Table

--- models/tag.js
+++ models/tag.js
@@ -1,36 +1,86 @@
 const pool = require('../config/db');
 
 const getAllTags = async () => {
-    const result = await pool.query('SELECT * FROM tag');
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM staffs');
+        await client.query('COMMIT');
+        return result.rows;
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getTagById = async (id) => {
-    const result = await pool.query('SELECT * FROM tag WHERE id = $1', [id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM staffs WHERE id = $1', [id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const createTag = async (tag) => {
     const { id, name } = tag;
-    const result = await pool.query(
-        'INSERT INTO tag (id, name) VALUES ($1, $2) RETURNING *',
-        [id, name]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'INSERT INTO staffs (id, name, role, salary, store) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+            [id, name, role, salary, store]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const updateTag = async (id, tag) => {
     const { name } = tag;
-    const result = await pool.query(
-        'UPDATE tag SET name = $1 WHERE id = $2 RETURNING *',
-        [name, id]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'UPDATE staffs SET name = $1, role = $2, salary = $3, store = $4 WHERE id = $5 RETURNING *',
+            [name, role, salary, store, id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const deleteTag = async (id) => {
-    const result = await pool.query('DELETE FROM tag WHERE id = $1 RETURNING *', [id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('DELETE FROM staffs WHERE id = $1 RETURNING *', [id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 module.exports = {
