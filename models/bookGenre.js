d1de24a50f96e54ec4f70d684e647c77794cdeaa
Awan <itproject2k22@gmail.com>
2024-06-06 08:18:30 +0700

    fix: add TCL to book_genre models

--- models/bookGenre.js
+++ models/bookGenre.js
@@ -1,35 +1,86 @@
 const pool = require('../config/db');
 
 const getAllBooksGenres = async () => {
-    const result = await pool.query('SELECT * FROM books_genre');
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM books_genre');
+        await client.query('COMMIT');
+        return result.rows;
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getBookGenreById = async (genre_id, books_id) => {
-    const result = await pool.query('SELECT * FROM books_genre WHERE genre_id = $1 AND books_id = $2', [genre_id, books_id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM books_genre WHERE genre_id = $1 AND books_id = $2', [genre_id, books_id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
-const createBookGenre = async (bookGenre) => {
-    const { genre_id, books_id } = bookGenre;
-    const result = await pool.query(
-        'INSERT INTO books_genre (genre_id, books_id) VALUES ($1, $2) RETURNING *',
-        [genre_id, books_id]
-    );
-    return result.rows[0];
+const createBookGenre = async (booksGenre) => {
+    const { genre_id, books_id } = booksGenre;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'INSERT INTO books_genre (genre_id, books_id) VALUES ($1, $2) RETURNING *',
+            [genre_id, books_id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
-const updateBookGenre = async (genre_id, books_id, bookGenre) => {
-    const result = await pool.query(
-        'UPDATE books_genre SET genre_id = $1, books_id = $2 WHERE genre_id = $3 AND books_id = $4 RETURNING *',
-        [bookGenre.genre_id, bookGenre.books_id, genre_id, books_id]
-    );
-    return result.rows[0];
+const updateBookGenre = async (genre_id, books_id, newBooksGenre) => {
+    const { new_genre_id, new_books_id } = newBooksGenre;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'UPDATE books_genre SET genre_id = $1, books_id = $2 WHERE genre_id = $3 AND books_id = $4 RETURNING *',
+            [new_genre_id, new_books_id, genre_id, books_id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const deleteBookGenre = async (genre_id, books_id) => {
-    const result = await pool.query('DELETE FROM books_genre WHERE genre_id = $1 AND books_id = $2 RETURNING *', [genre_id, books_id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('DELETE FROM books_genre WHERE genre_id = $1 AND books_id = $2 RETURNING *', [genre_id, books_id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 module.exports = {
