b2c282a037e9776d02814fa9c332d502afdd17ea
Awan <itproject2k22@gmail.com>
2024-06-06 10:22:27 +0700

    Fix: TCL in Tagging Table

--- models/tagging.js
+++ models/tagging.js
@@ -1,13 +1,33 @@
 const pool = require('../config/db');
 
 const getAllTaggings = async () => {
-    const result = await pool.query('SELECT * FROM tagging');
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM tagging');
+        await client.query('COMMIT');
+        return result.rows;
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getTaggingByBookId = async (book_id) => {
-    const result = await pool.query('SELECT * FROM tagging WHERE books_id = $1', [book_id]);
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM tagging WHERE books_id = $1', [book_id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getBooksByTagId = async (tag_id) => {
@@ -17,19 +37,39 @@ const getBooksByTagId = async (tag_id) => {
 
 const createTagging = async (tagging) => {
     const { tag_id, books_id } = tagging;
-    const result = await pool.query(
-        'INSERT INTO tagging (tag_id, books_id) VALUES ($1, $2) RETURNING *',
-        [tag_id, books_id]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await pool.query(
+            'INSERT INTO tagging (tag_id, books_id) VALUES ($1, $2) RETURNING *',
+            [tag_id, books_id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const deleteTagging = async (tag_id, books_id) => {
-    const result = await pool.query(
-        'DELETE FROM tagging WHERE tag_id = $1 AND books_id = $2 RETURNING *',
-        [tag_id, books_id]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await pool.query(
+            'DELETE FROM tagging WHERE tag_id = $1 AND books_id = $2 RETURNING *',
+            [tag_id, books_id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 module.exports = {
