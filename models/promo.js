9f2a34a2c28cc28ac9ebb36995da7aa0c08fb1f9
Awan <itproject2k22@gmail.com>
2024-06-06 10:21:29 +0700

    Fix: TCL in Promo Table

--- models/promo.js
+++ models/promo.js
@@ -1,36 +1,86 @@
 const pool = require('../config/db');
 
 const getAllPromos = async () => {
-    const result = await pool.query('SELECT * FROM promo');
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM promo');
+        await client.query('COMMIT');
+        return result.rows;
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getPromoById = async (id) => {
-    const result = await pool.query('SELECT * FROM promo WHERE id = $1', [id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM promo WHERE id = $1', [id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const createPromo = async (promo) => {
     const { id, name, description, start, finish } = promo;
-    const result = await pool.query(
-        'INSERT INTO promo (id, name, description, start, finish) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-        [id, name, description, start, finish]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'INSERT INTO promo (id, name, description, start, finish) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+            [id, name, description, start, finish]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const updatePromo = async (id, promo) => {
     const { name, description, start, finish } = promo;
-    const result = await pool.query(
-        'UPDATE promo SET name = $1, description = $2, start = $3, finish = $4 WHERE id = $5 RETURNING *',
-        [name, description, start, finish, id]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'UPDATE promo SET name = $1, description = $2, start = $3, finish = $4 WHERE id = $5 RETURNING *',
+            [name, description, start, finish, id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const deletePromo = async (id) => {
-    const result = await pool.query('DELETE FROM promo WHERE id = $1 RETURNING *', [id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('DELETE FROM promo WHERE id = $1 RETURNING *', [id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 module.exports = {
