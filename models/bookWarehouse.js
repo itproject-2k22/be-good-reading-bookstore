af02a86bf35472436dfc0f8ef6fa8fd50d1f6de8
Awan <itproject2k22@gmail.com>
2024-06-06 10:20:22 +0700

    fix: TCL in Book Warehouse Table

--- models/bookWarehouse.js
+++ models/bookWarehouse.js
@@ -1,36 +1,86 @@
 const pool = require('../config/db');
 
 const getAllBooksWarehouses = async () => {
-    const result = await pool.query('SELECT * FROM books_warehouse');
-    return result.rows;
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM books_warehouse');
+        await client.query('COMMIT');
+        return result.rows;
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const getBookWarehouseById = async (books_id, warehouse_id) => {
-    const result = await pool.query('SELECT * FROM books_warehouse WHERE books_id = $1 AND warehouse_id = $2', [books_id, warehouse_id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('SELECT * FROM books_warehouse WHERE books_id = $1 AND warehouse_id = $2', [books_id, warehouse_id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const createBookWarehouse = async (bookWarehouse) => {
     const { books_id, warehouse_id, quantity } = bookWarehouse;
-    const result = await pool.query(
-        'INSERT INTO books_warehouse (books_id, warehouse_id, quantity) VALUES ($1, $2, $3) RETURNING *',
-        [books_id, warehouse_id, quantity]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'INSERT INTO books_warehouse (books_id, warehouse_id, quantity) VALUES ($1, $2, $3) RETURNING *',
+            [books_id, warehouse_id, quantity]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const updateBookWarehouse = async (books_id, warehouse_id, bookWarehouse) => {
     const { quantity } = bookWarehouse;
-    const result = await pool.query(
-        'UPDATE books_warehouse SET quantity = $1 WHERE books_id = $2 AND warehouse_id = $3 RETURNING *',
-        [quantity, books_id, warehouse_id]
-    );
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query(
+            'UPDATE books_warehouse SET quantity = $1 WHERE books_id = $2 AND warehouse_id = $3 RETURNING *',
+            [quantity, books_id, warehouse_id]
+        );
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 const deleteBookWarehouse = async (books_id, warehouse_id) => {
-    const result = await pool.query('DELETE FROM books_warehouse WHERE books_id = $1 AND warehouse_id = $2 RETURNING *', [books_id, warehouse_id]);
-    return result.rows[0];
+    const client = await pool.connect();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query('DELETE FROM books_warehouse WHERE books_id = $1 AND warehouse_id = $2 RETURNING *', [books_id, warehouse_id]);
+        await client.query('COMMIT');
+        return result.rows[0];
+    } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 module.exports = {
